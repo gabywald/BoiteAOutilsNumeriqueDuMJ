69d0a26a965d798b2b1dc10548160c5a18dada38
Gaby Wald <30340425+gabywald@users.noreply.github.com>
2022-02-02 17:53:42 +0100

    [documentation][ressources][WIP]CyberPunk RED : completing data (2) FR !

---
+++ documentation/creationsPersonnages/CyberPunkTables/CyberPunkRED/biographicGeneration/database_org_fr/skills.js
@@ -0,0 +1,112 @@
+export default {
+    Perception: {
+        stat: "INT",
+        name: "Perception",
+        description: "Découvrir des objets caches, déchiffrer les émotions, reconnaître un mensonge"
+    },
+    Tracking: {
+        stat: "INT",
+        name: "Tracking",
+        description: "Suivre une piste"
+    },
+    Education: {
+        stat: "INT",
+        name: "Education",
+        description: "Culture générale, savoir scientifique, mathématique ou historique, actualité"
+    },
+    "Local Expert": {
+        stat: "INT",
+        name: "Local Expert",
+        description: "Connaître votre lieu de résidence, les factions présentes et leurs objectifs"
+    },
+    Interface: {
+        stat: "NET",
+        name: "Interface",
+        description: "Compétence spéciale de Piratage accessible uniquement aux Netrunners."
+    },
+    Marksmanship: {
+        stat: "REF",
+        name: "Marksmanship",
+        description: "Utiliser une arme à distance avec précision"
+    },
+    Driving: {
+        stat: "REF",
+        name: "Driving",
+        description: "Piloter des véhicules avec dextérité"
+    },
+    Evasion: {
+        stat: "DEX",
+        name: "Evasion",
+        description: "Esquiver les attaque que vous voyez venir"
+    },
+    Athletics: {
+        stat: "DEX",
+        name: "Athletics",
+        description: "Puissance musculaire, capacités athlétiques"
+    },
+    Stealth: {
+        stat: "DEX",
+        name: "Stealth",
+        description: "Se cacher et se mouvoir sans bruit"
+    },
+    Brawling: {
+        stat: "DEX",
+        name: "Brawling",
+        description: "Se battre en utilisant tous les membres de son corps"
+    },
+    Melee: {
+        stat: "DEX",
+        name: "Melee",
+        description: "Combattre avec des armes de mêlée"
+    },
+    "Basic Tech": {
+        stat: "TECH",
+        name: "Basic Tech",
+        description: "Identifier, comprendre et réparer des pièces électroniques"
+    },
+    Cybertech: {
+        stat: "TECH",
+        name: "Cybertech",
+        description: "Identifier, comprendre et réparer des implants cybernétiques"
+    },
+    "First Aid": {
+        stat: "TECH",
+        name: "First Aid",
+        description: "Panser vos plaies et celles des autres"
+    },
+    Bribery: {
+        stat: "COOL",
+        name: "Bribery",
+        description: "Savoir à qui graisser la patte, quand et pour quelle somme"
+    },
+    Interrogation: {
+        stat: "COOL",
+        name: "Interrogation",
+        description: "Soutirer des informations par la force"
+    },
+    Persuasion: {
+        stat: "COOL",
+        name: "Persuasion",
+        description: "Convaincre, persuader ou influencer les gens"
+    },
+    Concentration: {
+        stat: "WILL",
+        name: "Concentration",
+        description: "Se concentrer, faire appel à sa mémoire, sang-froid"
+    },
+    Conversation: {
+        stat: "EMP",
+        name: "Conversation",
+        description: "Soutirer des informations au détour des conversations"
+    },
+    "Human Perception": {
+        stat: "EMP",
+        name: "Human Perception",
+        description: "Savoir jouer d’un instrument et écrire des partitions"
+    },
+    "Play Instrument": {
+        stat: "EMP",
+        name: "Play Instrument",
+        description: "Déchiffrer le langage corporel pour décrypter les émotions"
+    }
+};
